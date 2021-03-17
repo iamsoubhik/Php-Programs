ae0d0694a6d44212fcfd6d175d2334b0d7592b80
Soubhik Mondal <soubhik.cse@gmail.com>
2021-03-17 12:06:01 +0530

    Basic Php

--- CRUD application/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn= mysqli_connect('localhost','root','','registration') OR die (mysqli_error());
-
-    if($conn){
-        echo "<br>";
-    }else{
-        echo "Error is found to connect";
-    }
-?>
